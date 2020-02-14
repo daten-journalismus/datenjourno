ee863a33740a55fda224d79748a5e805c114065d
Datascape On Demand <jieunk@live.com>
2020-02-14 21:34:33 +0100

    Updates

---
+++ precache-manifest.78913a60ab22cf07b0789752978f83ab.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ffc884f30ad9db0337b9cd0fa8b337",
+    "url": "/datenjourno/index.html"
+  },
+  {
+    "revision": "0a967f2f3787842dbbf8",
+    "url": "/datenjourno/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "20695d4a72a54a1b3695",
+    "url": "/datenjourno/static/css/main.75d3e4ca.chunk.css"
+  },
+  {
+    "revision": "0a967f2f3787842dbbf8",
+    "url": "/datenjourno/static/js/2.b2fe015f.chunk.js"
+  },
+  {
+    "revision": "f6a4203c5bf0a13c54ddb1287bef00fd",
+    "url": "/datenjourno/static/js/2.b2fe015f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20695d4a72a54a1b3695",
+    "url": "/datenjourno/static/js/main.b7d6dbc2.chunk.js"
+  },
+  {
+    "revision": "9175641d8b86fe7ff74d",
+    "url": "/datenjourno/static/js/runtime-main.6eb12bbf.js"
+  },
+  {
+    "revision": "8724ef2e464320d1eccbb0aa9d414b78",
+    "url": "/datenjourno/static/media/1.8724ef2e.png"
+  },
+  {
+    "revision": "3a39992044d626057da65954b523cd77",
+    "url": "/datenjourno/static/media/2.3a399920.png"
+  },
+  {
+    "revision": "30687241aacf44933d16dd2669e39767",
+    "url": "/datenjourno/static/media/3.30687241.png"
+  },
+  {
+    "revision": "8ffe26941792eb9f140922e97181be75",
+    "url": "/datenjourno/static/media/4.8ffe2694.png"
+  },
+  {
+    "revision": "b7d8a3886ea11bda4b3b5db2b0465085",
+    "url": "/datenjourno/static/media/5.b7d8a388.png"
+  },
+  {
+    "revision": "3ecb08323a9474209688d7193c5a13d1",
+    "url": "/datenjourno/static/media/6.3ecb0832.png"
+  },
+  {
+    "revision": "d5e8ce0746000e31ea9530d92337e1a1",
+    "url": "/datenjourno/static/media/7.d5e8ce07.png"
+  },
+  {
+    "revision": "38cd31aeac4357baa3cadc726756a447",
+    "url": "/datenjourno/static/media/8.38cd31ae.png"
+  },
+  {
+    "revision": "0a8209fa7b633c8cf0f8ce6355ae921e",
+    "url": "/datenjourno/static/media/about.0a8209fa.png"
+  },
+  {
+    "revision": "e878bfe641837b476a0e7d21aaae8271",
+    "url": "/datenjourno/static/media/welt_2019_topic_top68_wordcloud.e878bfe6.png"
+  },
+  {
+    "revision": "7e7176b0f18682e82adb3c35dddf3342",
+    "url": "/datenjourno/static/media/welt_monthly_topics_01_2020_pie.7e7176b0.png"
+  },
+  {
+    "revision": "99b4b043d69da5250561f34b59206a28",
+    "url": "/datenjourno/static/media/welt_weekly_art_desc_pos_tags_horiz.99b4b043.png"
+  },
+  {
+    "revision": "2a9802d0abcedfa52df79241c717d231",
+    "url": "/datenjourno/static/media/welt_weekly_art_desc_verbs_wordcloud.2a9802d0.png"
+  }
+]);
